b8eabc5658fbb6324a0d3cba6fb4233fbaf387d5
Arivan Amin <arivanamin@gmail.com>
2025-04-08 21:21:17 +0300

    Make Api Gateway scan core common controller advices

--- Api-Gateway/src/main/java/com/arivanamin/healthcare/backend/api/gateway/application/ApiGateway.java
+++ Api-Gateway/src/main/java/com/arivanamin/healthcare/backend/api/gateway/application/ApiGateway.java
@@ -4,7 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication
+import static com.arivanamin.healthcare.backend.base.domain.config.CoreApplicationConfig.BASE_PACKAGE;
+
+@SpringBootApplication (scanBasePackages = BASE_PACKAGE)
 @EnableDiscoveryClient
 public class ApiGateway {
 
