00563562a1369ddf69d4d617bc74e6d70b2bf329
Arivan Amin <arivanamin@gmail.com>
2025-04-15 21:18:55 +0300

    Enable JPA auditing

--- Audit-Service/src/main/java/com/arivanamin/healthcare/backend/audit/AuditApplication.java
+++ Audit-Service/src/main/java/com/arivanamin/healthcare/backend/audit/AuditApplication.java
@@ -4,12 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import static com.arivanamin.healthcare.backend.base.domain.config.CoreApplicationConfig.BASE_PACKAGE;
 
 @SpringBootApplication (scanBasePackages = BASE_PACKAGE)
 @EnableDiscoveryClient
 @EnableCaching
+@EnableJpaAuditing
 public class AuditApplication {
 
     public static void main (String[] args) {
--- Discovery-Server/src/main/java/com/arivanamin/healthcare/backend/discovery/application/DiscoveryServer.java
+++ Discovery-Server/src/main/java/com/arivanamin/healthcare/backend/discovery/application/DiscoveryServer.java
@@ -4,7 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@SpringBootApplication
+import static com.arivanamin.healthcare.backend.base.domain.config.CoreApplicationConfig.BASE_PACKAGE;
+
+@SpringBootApplication (scanBasePackages = BASE_PACKAGE)
 @EnableEurekaServer
 public class DiscoveryServer {
 
