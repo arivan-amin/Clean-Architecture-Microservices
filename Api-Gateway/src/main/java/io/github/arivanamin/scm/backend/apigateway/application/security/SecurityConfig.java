6a1f56a0051b2605420a2d4bd27fd52e8c91e30e
Arivan Amin <arivanamin@gmail.com>
2025-04-22 11:50:02 +0300

    Remove unnecessary zipkin dependency, tempo is responsible for distributed tracing

--- Api-Gateway/src/main/java/io/github/arivanamin/scm/backend/apigateway/application/security/SecurityConfig.java
+++ Api-Gateway/src/main/java/io/github/arivanamin/scm/backend/apigateway/application/security/SecurityConfig.java
@@ -1,11 +1,6 @@
 package io.github.arivanamin.scm.backend.apigateway.application.security;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.web.server.SecurityWebFilterChain;
-
-import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 class SecurityConfig {
@@ -14,14 +9,14 @@ class SecurityConfig {
         { "/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**", "/swagger-resources/**",
             "/api-docs/**", "/aggregate/**", "/actuator/prometheus" };
 
-    @Bean
-    public SecurityWebFilterChain securityFilterChain (ServerHttpSecurity httpSecurity) {
-        return httpSecurity.csrf(ServerHttpSecurity.CsrfSpec::disable)
-            .authorizeExchange(authorize -> authorize.pathMatchers(publicResourceUrls)
-                .permitAll()
-                .anyExchange()
-                .permitAll())
-            .oauth2ResourceServer(oauth2 -> oauth2.jwt(withDefaults()))
-            .build();
-    }
+    // @Bean
+    // public SecurityWebFilterChain securityFilterChain (ServerHttpSecurity httpSecurity) {
+    //     return httpSecurity.csrf(ServerHttpSecurity.CsrfSpec::disable)
+    //         .authorizeExchange(authorize -> authorize.pathMatchers(publicResourceUrls)
+    //             .permitAll()
+    //             .anyExchange()
+    //             .permitAll())
+    //         .oauth2ResourceServer(oauth2 -> oauth2.jwt(withDefaults()))
+    //         .build();
+    // }
 }
