beb05e142721f30371887146a8acd287487cbcf6
Arivan Amin <arivanamin@gmail.com>
2025-04-28 15:30:36 +0300

    Complete outbox functionality for sending audit events

--- Core/src/main/java/io/github/arivanamin/scm/backend/base/application/audit/KafkaAuditEventPublisher.java
+++ Core/src/main/java/io/github/arivanamin/scm/backend/base/application/audit/KafkaAuditEventPublisher.java
@@ -17,28 +17,15 @@ public class KafkaAuditEventPublisher implements AuditEventPublisher {
 
     @Override
     public void sendAuditLog (String topic, AuditEvent event) {
+        log.info("event in publisher to be sent = {}", event);
         kafkaTemplate.send(topic, event)
             .thenAccept(result -> {
                 updateCommand.execute(event.getId(), OutboxMessageStatus.SENT);
                 log.info("Audit event: {} sent successfully", event.getId());
             })
             .exceptionally(throwable -> {
-                log.error("Failed to send audit event: {}", event.getId(), throwable);
+                log.error("Failed to send audit event: {}", event, throwable);
                 return null;
             });
-        /*var completable = kafkaTemplate.send(topic, event);
-        completable.whenComplete((result, throwable) -> {
-            String formattedResult = result == null ? "" : result.toString();
-            String formattedThrowable = throwable == null ? "" : throwable.getMessage();
-            log.info("published audit event with result= {}, and throwable = {}", formattedResult,
-                formattedThrowable);
-            if (throwable == null) {
-                updateCommand.execute(event.getId(), OutboxMessageStatus.SENT);
-                // log.info("Audit event: {} sent successfully", event.getId());
-            }
-            if (result == null) {
-                // log.error("Failed to send audit event: {}", event.getId());
-            }
-        });*/
     }
 }
--- Core/src/main/java/io/github/arivanamin/scm/backend/base/application/outbox/AuditOutboxPublisherScheduler.java
+++ Core/src/main/java/io/github/arivanamin/scm/backend/base/application/outbox/AuditOutboxPublisherScheduler.java
@@ -3,7 +3,6 @@ package io.github.arivanamin.scm.backend.base.application.outbox;
 import io.github.arivanamin.scm.backend.base.domain.audit.AuditEvent;
 import io.github.arivanamin.scm.backend.base.domain.audit.AuditEventPublisher;
 import io.github.arivanamin.scm.backend.base.domain.audit.outbox.ReadAuditOutboxMessageByStatusQuery;
-import io.github.arivanamin.scm.backend.base.domain.audit.outbox.UpdateAuditOutboxMessageStatusCommand;
 import io.github.arivanamin.scm.backend.base.domain.outbox.OutboxMessageStatus;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -20,15 +19,12 @@ import static io.github.arivanamin.scm.backend.base.domain.topics.AuditTopics.AP
 public class AuditOutboxPublisherScheduler {
 
     private final ReadAuditOutboxMessageByStatusQuery query;
-    private final UpdateAuditOutboxMessageStatusCommand updateCommand;
     private final AuditEventPublisher publisher;
 
     @Scheduled (fixedRate = 10_000, initialDelay = 10_000)
     void execute () {
         List<AuditEvent> auditEvents = query.execute(OutboxMessageStatus.PENDING);
         log.info("Pending auditEvents to be published = {}", auditEvents.size());
-        auditEvents.forEach(event -> {
-            publisher.sendAuditLog(API_AUDIT_TOPIC, event);
-        });
+        auditEvents.forEach(event -> publisher.sendAuditLog(API_AUDIT_TOPIC, event));
     }
 }
