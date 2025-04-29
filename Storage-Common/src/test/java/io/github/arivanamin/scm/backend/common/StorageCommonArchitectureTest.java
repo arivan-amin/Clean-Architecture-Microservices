03bbfad5ab9bed14d9b106827647c103ad116435
Arivan Amin <arivanamin@gmail.com>
2025-04-29 14:48:53 +0300

    Add missing ArchUnit architecture test classes

---
+++ Storage-Common/src/test/java/io/github/arivanamin/scm/backend/common/StorageCommonArchitectureTest.java
@@ -0,0 +1,13 @@
+package io.github.arivanamin.scm.backend.common;
+
+import com.tngtech.archunit.core.importer.ImportOption;
+import com.tngtech.archunit.junit.AnalyzeClasses;
+import io.github.arivanamin.scm.backend.testing.architecture.rules.CleanArchitectureRules;
+import io.github.arivanamin.scm.backend.testing.architecture.rules.CommonBestPracticeRules;
+
+import static io.github.arivanamin.scm.backend.base.domain.config.CoreApplicationConfig.BASE_PACKAGE;
+
+@AnalyzeClasses (packages = BASE_PACKAGE, importOptions = ImportOption.DoNotIncludeTests.class)
+class StorageCommonArchitectureTest implements CommonBestPracticeRules, CleanArchitectureRules {
+
+}
