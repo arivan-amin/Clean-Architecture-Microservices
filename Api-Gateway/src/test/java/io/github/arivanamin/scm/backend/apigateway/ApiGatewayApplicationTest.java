a78f2046a853b79fa5c83a83360835b3a28b6f89
Arivan Amin <arivanamin@gmail.com>
2025-04-29 14:48:17 +0300

    Cleanup unnecessary files

--- Api-Gateway/src/test/java/io/github/arivanamin/scm/backend/apigateway/ApiGatewayApplicationTest.java
+++ Api-Gateway/src/test/java/io/github/arivanamin/scm/backend/apigateway/ApiGatewayApplicationTest.java
@@ -3,9 +3,7 @@ package io.github.arivanamin.scm.backend.apigateway;
 import io.github.arivanamin.scm.backend.testing.architecture.bases.BaseIntegrationTest;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 @Slf4j
 class ApiGatewayApplicationTest extends BaseIntegrationTest {
 
