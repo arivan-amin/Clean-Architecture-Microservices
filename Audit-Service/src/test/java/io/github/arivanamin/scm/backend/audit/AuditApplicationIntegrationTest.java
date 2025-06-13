b090da5a03630842cd7fe4545ecf48acc1ab34bb
Arivan Amin <arivanamin@gmail.com>
2025-06-13 22:17:28 +0300

    Rename main class tests

--- Audit-Service/src/test/java/io/github/arivanamin/scm/backend/audit/AuditApplicationIntegrationTest.java
+++ Audit-Service/src/test/java/io/github/arivanamin/scm/backend/audit/AuditApplicationIntegrationTest.java
@@ -5,7 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
 @Slf4j
-class AuditApplicationTest extends BaseDatabaseTest {
+class AuditApplicationIntegrationTest extends BaseDatabaseTest {
 
     @Test
     void mainShouldInitializeContext () {
--- Discovery-Server/src/test/java/io/github/arivanamin/scm/backend/discovery/DiscoveryServerApplicationIntegrationTest.java
+++ Discovery-Server/src/test/java/io/github/arivanamin/scm/backend/discovery/DiscoveryServerApplicationIntegrationTest.java
@@ -1,12 +1,11 @@
 package io.github.arivanamin.scm.backend.discovery;
 
+import io.github.arivanamin.scm.backend.testing.architecture.bases.BaseIntegrationTest;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 @Slf4j
-class DiscoveryServerApplicationTest {
+class DiscoveryServerApplicationIntegrationTest extends BaseIntegrationTest {
 
     @Test
     void mainShouldInitializeContext () {
