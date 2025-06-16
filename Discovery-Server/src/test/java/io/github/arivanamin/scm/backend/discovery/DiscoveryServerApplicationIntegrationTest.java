9f9ab7e0be0fb32f3102b6c30762f99232ff9e33
Arivan Amin <arivanamin@gmail.com>
2025-06-16 17:48:12 +0300

    Fix discovery server main integration test

--- Discovery-Server/src/test/java/io/github/arivanamin/scm/backend/discovery/DiscoveryServerApplicationIntegrationTest.java
+++ Discovery-Server/src/test/java/io/github/arivanamin/scm/backend/discovery/DiscoveryServerApplicationIntegrationTest.java
@@ -1,11 +1,16 @@
 package io.github.arivanamin.scm.backend.discovery;
 
-import io.github.arivanamin.scm.backend.testing.architecture.bases.BaseIntegrationTest;
+import io.github.arivanamin.scm.backend.testing.architecture.bases.BaseUnitTest;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @Slf4j
-class DiscoveryServerApplicationIntegrationTest extends BaseIntegrationTest {
+@SpringBootTest
+@ExtendWith (SpringExtension.class)
+class DiscoveryServerApplicationIntegrationTest implements BaseUnitTest {
 
     @Test
     void mainShouldInitializeContext () {
