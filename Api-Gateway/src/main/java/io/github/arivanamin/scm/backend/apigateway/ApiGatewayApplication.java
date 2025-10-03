d012b05d5fd7e6a27ee2c0d010cd9dddb4078b38
Arivan Amin <arivanamin@gmail.com>
2025-10-03 20:18:12 +0300

    Modify Eureka properties to not use field injection

--- Api-Gateway/src/main/java/io/github/arivanamin/scm/backend/apigateway/ApiGatewayApplication.java
+++ Api-Gateway/src/main/java/io/github/arivanamin/scm/backend/apigateway/ApiGatewayApplication.java
@@ -1,13 +1,16 @@
 package io.github.arivanamin.scm.backend.apigateway;
 
+import io.github.arivanamin.scm.backend.apigateway.application.config.EurekaProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-import static io.github.arivanamin.scm.backend.base.domain.config.CoreApplicationConfig.BASE_PACKAGE;
+import static io.github.arivanamin.scm.backend.base.core.config.CoreApplicationConfig.BASE_PACKAGE;
 
 @SpringBootApplication (scanBasePackages = BASE_PACKAGE)
 @EnableDiscoveryClient
+@EnableConfigurationProperties (EurekaProperties.class)
 public class ApiGatewayApplication {
 
     public static void main (String[] args) {
