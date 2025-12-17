68528a254acde0f1cd219d2c3f2e21e52ffa0c2d
Arivan Amin <arivanamin@gmail.com>
2025-12-17 22:19:30 +0300

    Fix EntityScan annotation new import

--- Audit-Service/src/main/java/io/github/arivanamin/scm/backend/audit/AuditApplication.java
+++ Audit-Service/src/main/java/io/github/arivanamin/scm/backend/audit/AuditApplication.java
@@ -2,7 +2,7 @@ package io.github.arivanamin.scm.backend.audit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
