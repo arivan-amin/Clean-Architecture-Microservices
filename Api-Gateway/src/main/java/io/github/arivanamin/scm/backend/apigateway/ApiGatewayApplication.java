fbb5fc43f529fab68fb2774b890b46c32da68748
Arivan Amin <arivanamin@gmail.com>
2026-01-11 11:11:03 +0300

    Refactor the api gateway eureka property configuration

--- Api-Gateway/src/main/java/io/github/arivanamin/scm/backend/apigateway/ApiGatewayApplication.java
+++ Api-Gateway/src/main/java/io/github/arivanamin/scm/backend/apigateway/ApiGatewayApplication.java
@@ -1,16 +1,13 @@
 package io.github.arivanamin.scm.backend.apigateway;
 
-import io.github.arivanamin.scm.backend.apigateway.application.config.EurekaProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 import static io.github.arivanamin.scm.backend.base.core.config.CoreApplicationConfig.BASE_PACKAGE;
 
 @SpringBootApplication (scanBasePackages = BASE_PACKAGE)
 @EnableDiscoveryClient
-@EnableConfigurationProperties (EurekaProperties.class)
 public class ApiGatewayApplication {
 
     static void main (String[] args) {
--- Api-Gateway/src/main/java/io/github/arivanamin/scm/backend/apigateway/application/config/EurekaProperties.java
+++ Api-Gateway/src/main/java/io/github/arivanamin/scm/backend/apigateway/application/config/EurekaProperties.java
@@ -2,7 +2,7 @@ package io.github.arivanamin.scm.backend.apigateway.application.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
-@ConfigurationProperties (prefix = "eureka")
+@ConfigurationProperties (prefix = "scm.eureka")
 public record EurekaProperties(String host, String port) {
 
 }
