ecc7c9bb5963604c05f5c213f1817051fcd14d4a
Arivan Amin <arivanamin@gmail.com>
2026-01-23 22:03:42 +0300

    Rename discovery server and api gateway

--- Discovery-Server/src/main/java/io/github/arivanamin/scm/backend/discovery/application/config/EurekaCredentialProperties.java
+++
@@ -1,8 +0,0 @@
-package io.github.arivanamin.scm.backend.discovery.application.config;
-
-import org.springframework.boot.context.properties.ConfigurationProperties;
-
-@ConfigurationProperties (prefix = "scm.eureka.credentials")
-public record EurekaCredentialProperties(String username, String password) {
-
-}
