b5a4bdd75b1434bce79de2b9f534d1c80c601d47
Arivan Amin <arivanamin@gmail.com>
2026-02-18 14:48:34 +0300

    Remove java faker from tests

--- Testing/src/main/java/io/github/arivanamin/lms/backend/testing/architecture/bases/BaseUnitTest.java
+++ Testing/src/main/java/io/github/arivanamin/lms/backend/testing/architecture/bases/BaseUnitTest.java
@@ -1,14 +1,11 @@
 package io.github.arivanamin.lms.backend.testing.architecture.bases;
 
-import com.github.javafaker.Faker;
 import io.github.arivanamin.lms.backend.core.domain.pagination.PageData;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith (MockitoExtension.class)
 public interface BaseUnitTest {
-
-    Faker FAKER = new Faker();
 
     PageData PAGE_DATA = PageData.of(0, 5, 5, 25);
 }
