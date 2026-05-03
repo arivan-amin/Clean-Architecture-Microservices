33b1f96cb89f1c2fe972c71ba5f9a107330ab370
Arivan Amin <arivanamin@gmail.com>
2026-05-03 20:46:24 +0300

    Cleanup old files

--- cinemayan-core/src/main/java/com/cinemayan/core/application/audit/AuditSensitiveProperties.java
+++ cinemayan-core/src/main/java/com/cinemayan/core/application/audit/AuditSensitiveProperties.java
@@ -1,13 +1,17 @@
 package com.cinemayan.core.application.audit;
 
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
 
 import java.util.Set;
 
 @ConfigurationProperties (prefix = "audit.masking.sensitive")
+@Validated
 @Value
 class AuditSensitiveProperties {
 
+    @NotEmpty
     Set<String> fields;
 }
